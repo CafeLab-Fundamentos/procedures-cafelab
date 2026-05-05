8b175b017d5f4afaff348ebeb4828235ee36a2fa
natalia <natalia.roman.cruz@gmail.com>
2026-05-05 16:29:22 -0500

    Initial commit

---
+++ src/main/java/com/acme/procedurescafelab/ProceduresCafeLabApplication.java
@@ -0,0 +1,13 @@
+package com.acme.procedurescafelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProceduresCafeLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProceduresCafeLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/procedurescafelab/ProceduresCafeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.procedurescafelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProceduresCafeLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
